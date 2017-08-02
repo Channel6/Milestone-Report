03ab8f8bf7ea31473ef9cdc009b10d89c58aedfb
Glenn Kerbein <glenn@spontaneousdancing.net>
2017-08-01 20:30:17 -0700

    flesh out summary stats

--- Milestone-Report.R
+++ Milestone-Report.R
@@ -15,7 +15,7 @@ if (!file.exists("./data/final")) {
 }
 ###only English loading
 blogs <- readLines("./data/final/en_US/en_US.blogs.txt", encoding = "UTF-8", skipNul=TRUE)
-twitter <-  readLines("./data/final/en_US/en_US.twitter.txt", encoding = "UTF-8", skipNul=TRUE)
+tweets <-  readLines("./data/final/en_US/en_US.twitter.txt", encoding = "UTF-8", skipNul=TRUE)
 
 # News dataset looks like a raw binary file to R. To mitigate this, open the file as a stream,
 # then read the data, line by line, into a new variable.
@@ -26,11 +26,68 @@ rm(con)
 
 ### Grab sample data.
 head(blogs, 10)
-head(twitter, 5)
+head(tweets, 5)
 head(news, 5)
 
 ## Basic report of summary statistics about the data sets
 
+# Compute words per line info on each line for each data type
+rawWPL<-lapply(
+		list(blogs,news,twits),
+		function(x) stri_count_words(x)
+	)
+
+# Compute statistics and summary info for each data type
+rawstats<-data.frame(
+	File=c("blogs","news","tweets"),
+	t(
+		rbind(
+			sapply(
+				list(blogs,news,twits),
+				stri_stats_general
+			),
+			TotalWords=sapply(
+				list(blogs,news,twits),
+				stri_stats_latex
+			)[4,]
+		)
+	),
+	# Compute words per line summary
+	WPL=rbind(
+		summary(rawWPL[[1]]),
+		summary(rawWPL[[2]]),
+		summary(rawWPL[[3]])
+	)
+)
+print(rawstats)
+
+# Plot histogram for each data type
+qplot(
+	rawWPL[[1]],
+	geom="histogram",
+	main="Histogram for US Blogs",
+	xlab="No. of Words",
+	ylab="Frequency",
+	binwidth=10
+)
+qplot(
+	rawWPL[[2]],
+	geom="histogram",
+	main="Histogram for US News",
+	xlab="No. of Words",
+	ylab="Frequency",
+	binwidth=10
+)
+qplot(
+	rawWPL[[3]],
+	geom="histogram",
+	main="Histogram for US Tweets",
+	xlab="No. of Words",
+	ylab="Frequency",
+	binwidth=1
+)
+
+rm(rawWPL);rm(rawstats)
 
 ## Report any interesting findings that you amassed so far
 
